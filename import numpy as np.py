e7430626074d4aac7b5c983c13a1db3d85dc4a7d
Jaswanth Remiel AG <66111735+JaswanthRemiel@users.noreply.github.com>
2023-02-15 16:25:26 +0530

    first commit

---
+++ import numpy as np.py
@@ -0,0 +1,3 @@
+import numpy as np
+B = [True , 2,3.0,np.nan,"False"]
+print([type(i) for i in B])
---
+++ pythong.py
@@ -0,0 +1,4 @@
+import numpy as np
+arr = np.array([[1, 9, 10], [3, 7, 6], [12, 8, 0]])
+print(arr)
+print( np.sum(arr, axis = 1))
